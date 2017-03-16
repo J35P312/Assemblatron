e0bc2c66b41ea7c685e1af839fa0cdcf3cc1c6a5
J35P312 <jesper.eisfeldt@scilifelab.se>
2017-03-16 14:20:44 +0100

    new file:   coverage_analysis.sh

---
+++ coverage_analysis.sh
@@ -0,0 +1,16 @@
+#analyse the coverage across the genome
+#first argument bam file of aligned contigs
+#second argument - a gene list
+#third argument output prefix
+#requires bedtools and tiddit =P
+
+TIDDIT_path=./TIDDT
+
+$TIDDIT_path --cov -b $1 -o $3
+python compute_coverage.py $3.tab  > $3_genome_wide.tab
+
+intersectBed -a $3.tab -b $2 > $3.panel.tab
+python compute_coverage.py $3.tab  > $3_panel_wide.tab
+
+
+
